24d1a4e960a4e84057998bdb474a9a07bd9ee586
Welliton <josealves_w@yahoo.com>
2020-01-18 23:49:59 -0300

    moving the bird wings

--- core/src/com/cursoandroid/flappybird/Jogo.java
+++ core/src/com/cursoandroid/flappybird/Jogo.java
@@ -6,22 +6,32 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Jogo extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture img;
+	private Texture passaros [];
 	private Texture fundo;
 	private int movimentox = 0;
 	private int movimentoy = 0;
+
+
 	private float larguraDispositivo;
 	private float alturaDispositivo;
+	private float variacao;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("passaro1.png");
+		passaros =  new Texture[3];
+		passaros [0] = new Texture("passaro1.png");
+		passaros [1] = new Texture("passaro2.png");
+		passaros [2] = new Texture("passaro3.png");
 		fundo  =  new Texture("fundo.png");
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
+		variacao = 0;
 	}
 
 	@Override
@@ -30,13 +40,19 @@ public class Jogo extends ApplicationAdapter {
 		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(fundo, 0, 0,larguraDispositivo, alturaDispositivo);
-		batch.draw(img, movimentox++, movimentoy++);
+		batch.draw(passaros[(int)variacao], movimentox++, movimentoy++);
 		batch.end();
+
+		variacao += Gdx.graphics.getDeltaTime() * 10;
+
+
+		if(variacao >= 3){
+			variacao = 0.0f;
+		}
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
